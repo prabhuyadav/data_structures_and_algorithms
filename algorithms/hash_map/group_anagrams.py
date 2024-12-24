034101190eb25df8c5add451406057a9d3493fb2
prabhuyadav <prabhuyadav007.py@gmail.com>
2024-12-24 22:27:08 +0530

    group anagrams.

---
+++ algorithms/hash_map/group_anagrams.py
@@ -0,0 +1,23 @@
+# question link -> https://leetcode.com/problems/group-anagrams/description/?envType=study-plan-v2&envId=top-100-liked
+
+"""
+Solution - Time O(n * mlog(m)) | Space O(n * m)
+
+- initialize a map that we are going to use to keep track of sorted string against all the strings which are anagrams of this sorted key.
+- we are gonna start a loop through the given input array of strings.
+- for every string we sort it, check if this sorted value is present in the map as a key - if yes add the original string into the array of values.
+- otherwise add the sorted string as a new key in the map with a value of [originalStr].
+- once the process is done - return the values of the map in an array.
+
+"""
+
+def groupAnagrams(strs: list[str]) -> list[list[str]]:
+    anagramsMap = {}
+    for originalStr in strs:
+        sortedStr = ''.join(sorted(originalStr))
+        if sortedStr in anagramsMap:
+            anagramsMap[sortedStr].append(originalStr)
+        else:
+            anagramsMap[sortedStr] = [originalStr]
+
+    return anagramsMap.values()
